c8b9f535ba3c3dfcc74a192cc41bda7bcfcd94e4
Matheus Pozett <maa_souza11@hotmail.com>
2025-07-08 20:25:23 -0300

    feat: cria classe PessoaEleitora com atributo cpf para armazenar o cpf da pessoa em string

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,26 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * The type Pessoa eleitora.
+ */
+public class PessoaEleitora extends Pessoa{
+  private String cpf;
 
+  /**
+   * Gets cpf.
+   *
+   * @return the cpf
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Sets cpf.
+   *
+   * @param cpf the cpf
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
