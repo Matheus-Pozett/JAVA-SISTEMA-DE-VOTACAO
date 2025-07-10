7e753d8e649f9a8c61d22f04ec8c844bb9c72505
Matheus Pozett <maa_souza11@hotmail.com>
2025-07-10 19:41:45 -0300

    fix: ajusta checkstyle

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,15 +1,17 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * The type Gerenciamento votacao.
+ */
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
   private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
   private ArrayList<String> cpfsComputados = new ArrayList<>();
 
-
   @Override
   public void cadastrarPessoaCandidata(String nome, int numero) {
-    if(pessoasCandidatas.stream().anyMatch(p -> p.getNumero() == numero)) {
+    if (pessoasCandidatas.stream().anyMatch(p -> p.getNumero() == numero)) {
       System.out.println("Número da pessoa candidata já utilizado!");
     } else {
       PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
@@ -19,7 +21,7 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void cadastrarPessoaEleitora(String nome, String cpf) {
-    if(pessoasEleitoras.stream().anyMatch(p -> p.getCpf().equals(cpf))) {
+    if (pessoasEleitoras.stream().anyMatch(p -> p.getCpf().equals(cpf))) {
       System.out.println("Pessoa eleitora já cadastrada!");
     } else {
       PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -3,8 +3,16 @@ package com.betrybe.sistemadevotacao;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * The type Principal.
+ */
 public class Principal {
 
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
